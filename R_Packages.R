f4ee79c17c35fb54464bef1f517ed222a27b348d
Jon Chua <jon.chua4@gmail.com>
2018-04-26 17:04:13 -0500

    New Package file

---
+++ R_Packages.R
@@ -0,0 +1,36 @@
+#### This code installs or updates some commonly used packages
+
+packageNames = c(
+  "glmnet", 
+  "class", 
+  "recommenderlab",
+  "devtools",
+  "shiny",
+  "tidyverse",
+  "knitr",
+  "readxl",
+  "lubridate",
+  "broom",
+  "hms",
+  "XML",
+  "kknn",
+  "tree",
+  "randomForest",
+  "rpart",
+  "gbm",
+  "acepack",
+  "data.table",
+  "rpart.plot",
+  "ROCR",
+  "lift",
+  "caret",
+  "dplyr",
+  "baseballr"
+  )
+
+for (pkgName in packageNames) {
+  if(!(pkgName %in% rownames(installed.packages()))) {
+    install.packages(pkgName, dependencies=TRUE)
+  }
+}
+update.packages(ask=FALSE)
